598ebfbb03f19818e4ef7c48725fcb5568a0314b
cristianaquino <ycristian10@gmail.com>
2022-02-03 22:07:40 -0500

    prueba

---
+++ Codigo/Prueba.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+	int a,b;
+	cout<<"HOLA MUNDO";
+	for (int i = 0; i < a; i++)
+	{
+		i=i+1;
+	}
+	cout<<"HELLO WORLD";
+	/*hola como estan XD*/
+	while (a<0)
+	{
+		a=a*a;
+	}
+	for (int j = 0; j < a; j++)
+	{
+		for (int k = 0; k < b; k++)
+		{
+			/* code */
+		}
+	}
+	/*COMENTANDO DENUEVO*/
+	do
+	{
+		a=a*b;
+	} while (b=0);
+	cin>>a;
+	cin>>b;
+	/*COMENTANDO POR TERCERA VEZ*/
+
+	void Velocidad(){
+		int c,d=0;
+		d=c+d;
+	}
+
+	void Aceleracion(){
+		ind e,f=0;
+		f=e+f;
+	}
+	/*Comentando por cuarta vez*/
+	return 0;
+}
